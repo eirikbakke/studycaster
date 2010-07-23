15a055baffd0b34f7b04c10277e21c42ba44a765
Eirik Bakke <ebakke@mit.edu>
2010-07-23 01:33:50 -0400

    Added some experiments with Win32 window info functions. This commit mainly to transfer to desktop machine.

--- SCNative/NativeLibrary.c
+++ SCNative/NativeLibrary.c
@@ -1,5 +1,23 @@
+#include <windows.h>
+#include <stdio.h>
 #include "NativeLibrary.h"
 
+static BOOL CALLBACK myEnumWindowsProc(HWND hwnd, LPARAM lParam) {
+  int len = GetWindowTextLength(hwnd);
+  //printf("got this far, length was %d", len);
+  if (len > 0) {
+    char buf[len + 1];
+    //printf("so %d", sizeof(*buf));
+    if (GetWindowText(hwnd, buf, len + 1) > 0) {
+      printf("The window text is \"%s\"\n", buf);
+    }
+  }
+
+  return TRUE;
+}
+
 JNIEXPORT jstring JNICALL Java_no_ebakke_studycaster2_NativeLibrary_getTestString(JNIEnv *env, jclass clazz) {
-  return (*env)->NewStringUTF(env, "Hello world from JNI (test string4)!");
+  BOOL res = EnumWindows(&myEnumWindowsProc, (LPARAM) NULL);
+  printf("EnumWindows returned %s\n", res ? "true" : "false");
+  return (*env)->NewStringUTF(env, "Hello world from JNI (test string5)!");
 }
