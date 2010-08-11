801ef4325fc62a852054448c9a7266b369a33609
Eirik Bakke <ebakke@mit.edu>
2010-08-11 01:13:42 -0400

    Started framework for an XML-configurable StudyCaster version.

---
+++ src/no/ebakke/studycaster/applications/ConfigurableStudyCaster.java
@@ -0,0 +1,47 @@
+package no.ebakke.studycaster.applications;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import no.ebakke.studycaster.api.StudyConfiguration;
+import no.ebakke.studycaster.api.ServerContext;
+import no.ebakke.studycaster.api.StudyCasterException;
+
+public class ConfigurableStudyCaster {
+  public static Logger log = Logger.getLogger("no.ebakke.studycaster");
+  private StudyConfiguration configuration;
+  private ServerContext serverContext;
+
+  public ConfigurableStudyCaster(URI serverURI, String configurationFileName) throws StudyCasterException {
+    serverContext = new ServerContext(serverURI);
+    try {
+      configuration = new StudyConfiguration(serverContext.downloadFile(configurationFileName));
+    } catch (IOException e) {
+      throw new StudyCasterException("Error retrieving configuration file", e);
+    }
+  }
+
+  public static void main(String args[]) {
+    ConfigurableStudyCaster csc;
+    URI uri;
+
+    if (args.length != 2) {
+      System.err.println("Usage: ConfigurateStudyCaster <server script URI> <remote configuration file name>");
+      return;
+    }
+    try {
+      uri = new URI(args[0]);
+    } catch (URISyntaxException e) {
+      System.err.println("Malformed URI");
+      return;
+    }
+    try {
+      csc = new ConfigurableStudyCaster(uri, args[1]);
+    } catch (StudyCasterException e) {
+      log.log(Level.SEVERE, "Failed to initialize StudyCaster", e);
+      return;
+    }
+  }
+}
