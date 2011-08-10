c063edfbc8d42acf6c83d4568b9d222aa0feb9dd
Eirik Bakke <ebakke@mit.edu>
2011-08-09 19:58:13 -0700

    Created and configured NetBeans client and server projects. Added a sample main class/servlet in correct package. Configured ignore. Made a common library folder.

---
+++ StudyCasterClient/src/no/ebakke/studycaster/driver/Main.java
@@ -0,0 +1,9 @@
+package no.ebakke.studycaster.driver;
+
+public final class Main {
+  private Main() { }
+  
+  public static void main(String args[]) {
+    System.out.println("Hello, World!");
+  }
+}
