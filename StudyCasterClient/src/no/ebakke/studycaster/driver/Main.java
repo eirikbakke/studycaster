c528948f114e5940e1ab7dbc06f3d4b1a0f3dfad
Eirik Bakke <ebakke@mit.edu>
2011-08-11 19:36:18 -0700

    Included an alternative implementation of the window enumerator using the JNA library.

--- StudyCasterClient/src/no/ebakke/studycaster/driver/Main.java
+++ StudyCasterClient/src/no/ebakke/studycaster/driver/Main.java
@@ -1,9 +1,12 @@
 package no.ebakke.studycaster.driver;
 
+import no.ebakke.screencasting.WindowEnumerator;
+
 public final class Main {
   private Main() { }
   
   public static void main(String args[]) {
     System.out.println("Hello, World!");
+    WindowEnumerator.test();
   }
 }
